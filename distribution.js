26a11a11f4587d6d856abeb2b719103e81d45208
FRGM <frgm88@gmail.com>
2018-02-28 22:39:37 -0300

    Graphs, Google Maps integration

--- distribution.js
+++ distribution.js
@@ -1,9 +1,9 @@
 $(function() {
-    var localList = {"test Location 1":{address:"test address", coords:{lat: 37.419347, lng: -122.079975}, items:{"testItem1":100, "testItem2":50}}, 
-                     "test Location 3":{address:"test address 2", coords:{lat: 37.419347, lng: -122.079975}, items:{"testItem4":100, "testItem3":50}}}; //change this
+    localList = {"testLocation1":{address:"test address", coords:{lat: 37.419347, lng: -122.079975}, items:{"testItem1":100, "testItem2":50}}, 
+                     "testLocation3":{address:"test address 2", coords:{lat: 37.419347, lng: -122.079975}, items:{"testItem4":100, "testItem3":50}}}; //change this
     var dateFormat = { year: 'numeric', month: 'short', day: '2-digit'};
     today = new Intl.DateTimeFormat('es-CL',dateFormat).format(new Date());
-    makeForm(localList);
+    makeForm(localList);    
 });
 
 function makeForm(locals){
@@ -12,9 +12,7 @@ function makeForm(locals){
     $.each(locals, function(key, value){
         $location = $('<div id="div'+ key +'" class="divLocation sliding" style="display: none"></div>');
         $location.append('<label class="lblAddress">'+ value.address +'</label></br>');
-        //google map here
-        $location.append('<div id="div'+ key +'_map" class="divLocation sliding" style="display: none"></div>'
-        ////////
+        $location.append('<div id="div'+ key +'_map" style="width: 300px; height: 200px;"></div>');
         $location.append('</br>')
         $location.append('<label class="lblLocation">En este local hoy"'+ d +'"debes entregar:</label></br>');
         $items = $('<div id="divItems'+ key +'" class="divItems"></div>');
@@ -28,13 +26,19 @@ function makeForm(locals){
         });
         $("#divDistribution").append($label)
         $("#divDistribution").append($location)
-        $("#divDistribution").append("</br></br></br>") //Browser render bug? Compensate for missing space
+        $("#divDistribution").append("</br></br></br>") //Browser render bug? Compensate for missing space      
     });
 }
-function initMap(){
-    var map = new google.maps.Map(document.getElementById('div'+ key), {
+function makeMap(div, coords){
+    var map = new google.maps.Map(document.getElementById(div), {
         zoom: 18,
-        center: value.coords,
+        center: coords,
         mapTypeId: 'terrain'
     });
 }        
+
+function initMaps(){
+    $.each(localList, function(key, value){
+        makeMap("div"+ key +"_map", value.coords);
+    });
+}
