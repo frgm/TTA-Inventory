29ad7708641d082cec7ecc2aeb681bb5d8089224
FRGM <frgm88@gmail.com>
2018-03-06 22:02:24 -0300

    Django Integration

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('adminInv', views.adminInv, name='adminInv'),
+    path('adminPro', views.adminPro, name='adminPro'),
+    path('distribution', views.distribution, name='distribution'),
     path('', views.index, name='index'),
+    path('production', views.production, name='production'),
+    path('report', views.report, name='report')
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
+def adminInv(request):
+    return render_to_response("adminInv.html")
+    
+def adminPro(request):
+    return HttpResponse("adminPro")
+
+def distribution(request):
+    return HttpResponse("distribution")
+
 def index(request):
-    return HttpResponse("Hello, world. You're at index.")
+    return HttpResponse("I N D E X")
+
+def production(request):
+    return HttpResponse("production")
+
+def report(request):
+    return HttpResponse("report")
