27cdbb7bf92299b9a3aac30e2cc5c6f43d937c74
FRGM <frgm88@gmail.com>
2018-03-22 18:30:25 -0300

    Databse migration test

--- TTA_Inventory/settings.py
+++ TTA_Inventory/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'TTA_Inventory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
