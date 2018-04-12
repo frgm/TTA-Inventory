9becd0c02da235ba0268248345440f31af74dddf
FRGM <frgm88@gmail.com>
2018-04-12 16:54:25 -0300

    Menu debug

--- mainApp/static/menu.js
+++ mainApp/static/menu.js
@@ -11,12 +11,13 @@ $(function(){
     }).done(function(response){
         if(response.success){
             flags = parseInt(response.role);
+            alert(flags);
+            alert(flags & 8);
             if (flags & 16){    //adminPro
                 //$("#adminPro").bind('click', {param: "adminPro"}, menuClick);
-                alert("set" + flags & 16);
+                pass;
             }else if (flags & 8){   //adminInv
                 $("#adminInv").bind('click', {param: "adminInv"}, menuClick);
-                alert("set" + flags & 8);
             }else if (flags & 4){   //distribution
                 $("#distribution").bind('click', {param: "distribution"}, menuClick);
             }else if (flags & 2){   //production
